b83fa171f28a57b11a61150ec6f0c95b514099f4
Zhuang Jun Kai <113646306+KingKaiZhuang@users.noreply.github.com>
2024-06-12 11:25:16 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My React Final Test Project`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
